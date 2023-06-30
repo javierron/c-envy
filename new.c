a4beff5aa45aff66c1ebe0dfc0c2bab309ec7f5d
Javier Ron Arteaga <javierron90@gmail.com>
2023-06-30 14:40:48 +0200

    source fork
    Signed-off-by: Javier Ron Arteaga <javierron90@gmail.com>

--- new.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int very_important_function(char data[], int length){
-    printf("performing operation of high relevance on data!\n");
-    printf("data: %s | length: %d \n", data, length);
-    return 0;
-}int main(){
-    very_important_function("Hello World!", 12);
-}
--- preprocess.py
+++ preprocess.py
@@ -1,22 +1,31 @@
+#!/bin/python3
 import json
 
 def main():
+    data = {}
     with open('function_data.json', 'r') as f:
         data = json.load(f)
         
-        lines = []
-        var_lines = []
+    lines = []
+    var_lines = []
+    
+    with open(data['file'], 'r') as src:
+        lines = src.readlines()
+
+    with open(data['variant'], 'r') as var:
+        var_lines = var.readlines()
+            
+    for i in range(len(lines)):
+        lines[i] = lines[i].replace(data['name'], 'original')
+
+    for i in range(len(var_lines)):
+        var_lines[i] = var_lines[i].replace(data['name'], 'variant')
         
-        with open(data["file"], 'r') as src:
-            lines = src.readlines()
+    l = lines[:data['line'] + data['size']] + var_lines + lines[data['line'] + data['size']:data['call_line'] + data['call_size'] - 1] + [lines[data['call_line'] - 1].replace('original', 'variant')] + lines[data['call_line'] + data['call_size'] - 1:]
 
-        with open(data["variant"], 'r') as var:
-            var_lines = var.readlines()
-                
-        lines = lines[:data["line"] -1 ] + var_lines + lines[data["line"] + data["size"] - 1:]
-
-        with open(data["file"], 'w') as n:
-            n.writelines(lines)
+    with open(data["file"], 'w') as n:
+    # with open('new.c', 'w') as n:
+        n.writelines(l)
 
 if __name__ == "__main__":
     main()
